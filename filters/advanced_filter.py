c2a98e82724558d84a649c92441d0e37a005472d
Siu Kim <aforever2731@gmail.com>
2026-05-26 14:06:39 +0900

    Update advanced_filter.py

--- filters/advanced_filter.py
+++ filters/advanced_filter.py
@@ -2,13 +2,26 @@ from lingua import Language, LanguageDetectorBuilder
 from filters.base_filter import BaseFilter
 
 class LanguageFilter(BaseFilter):
-    def __init__(self):
-        self.judge = LanguageDetectorBuilder.from_all_languages().build()
+    def __init__(self, threshold=0.6, fast_drop=10):
+        self.judge = LanguageDetectorBuilder.from_languages(
+            Language.KOREAN,
+            Language.JAPANESE,
+            Language.CHINESE, 
+            Language.ENGLISH
+        ).build()
+        self.threshold = threshold
+        self.fastdrop = fast_drop
 
     def apply(self, text: str):
-        match = self.judge.detect_language_of(text)
+        if len(text.strip()) < self.fastdrop:
+            return False
+        
+        match = self.judge.compute_language_confidence(text, Language.KOREAN)
 
-        if match.iso_code_639_1.name == "KO":
+        if match is None:
+            return False
+        
+        if match >= self.threshold:
             return True
         else:
             return False
