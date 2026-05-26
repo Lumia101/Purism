1c91cfcd486eff6e9c39b6e87590f85e1ca435e1
Siu Kim <aforever2731@gmail.com>
2026-05-26 18:08:15 +0900

    Update pipeline.py

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -2,6 +2,7 @@
 from normalizers.normalizer import TextCleaner, UICleaner, UnicodeCleaner
 from filters.simple_filter import LengthFilter, HarmfulWordsFilter, SpamWordsFilter, SignAbuseFilter, PIIFilter
 from filters.advanced_filter import LanguageFilter
+from filters.model_filter import PPLFilter
 
 # Setting Settings for Data Purification
 class PurifyConfig():
