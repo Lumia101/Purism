7b3c2fee5e8efed1123d8dba34d7d1090b84fd24
Siu Kim <aforever2731@gmail.com>
2026-05-26 14:16:34 +0900

    Update advanced_filter.py

--- filters/advanced_filter.py
+++ filters/advanced_filter.py
@@ -1,21 +1,19 @@
+# Load libraries
 from lingua import Language, LanguageDetectorBuilder
 from filters.base_filter import BaseFilter
 
+# Use the lingua library to remove non-Korean sentences
 class LanguageFilter(BaseFilter):
-    def __init__(self, threshold=0.6, fast_drop=10):
+    def __init__(self, threshold=0.6):
         self.judge = LanguageDetectorBuilder.from_languages(
             Language.KOREAN,
             Language.JAPANESE,
             Language.CHINESE, 
             Language.ENGLISH
         ).build()
-        self.threshold = threshold
-        self.fastdrop = fast_drop
+        self.threshold = threshold # 
 
     def apply(self, text: str):
-        if len(text.strip()) < self.fastdrop:
-            return False
-        
         match = self.judge.compute_language_confidence(text, Language.KOREAN)
 
         if match is None:
