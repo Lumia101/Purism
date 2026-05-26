146f91920c49ad0feb0f8059e6a28fdee92b6fe0
Siu Kim <aforever2731@gmail.com>
2026-05-26 11:17:18 +0900

    Update advanced_filter.py

--- filters/advanced_filter.py
+++ filters/advanced_filter.py
@@ -2,14 +2,13 @@ from lingua import Language, LanguageDetectorBuilder
 from filters.base_filter import BaseFilter
 
 class LanguageFilter(BaseFilter):
-    def __init__(self, threshold=0.5):
+    def __init__(self):
         self.judge = LanguageDetectorBuilder.from_languages(Language.KOREAN).build()
-        self.threshold = threshold
 
     def apply(self, text: str):
-        ratio = self.judge.compute_language_confidence(text, Language.KOREAN)
+        match = self.judge.detect_language_of(text)
 
-        if ratio < self.threshold:
-            return False
-        else:
+        if match == Language.KOREAN:
             return True
+        else:
+            return False
