46ff3fac455a94a5c34115cfed38de9b8955207e
Siu Kim <aforever2731@gmail.com>
2026-05-28 19:34:38 +0900

    Update model_filter.py

--- purism/filters/model_filter.py
+++ purism/filters/model_filter.py
@@ -5,23 +5,29 @@ from .base_filter import BaseFilter
 # Measure the PPL of corpus and filter corpus with excessively high PPL.
 class PPLFilter(BaseFilter):
     def __init__(self, ppl_threshold=180.0):
-        model_id = "LiquidAI/LFM2.5-1.2B-Instruct"
-        self.quant_config = BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_quant_type="nf4",
-            bnb_4bit_use_double_quant=True
-        )
-        self.model = AutoModelForCausalLM.from_pretrained(
-            model_id,
-            device_map="auto",
-            dtype="auto",
-            quantization_config=self.quant_config
-        )
-        self.model.eval()
-        
-        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
-        self.drop_ratio = ppl_threshold
+        self.model_id = "LiquidAI/LFM2.5-1.2B-Instruct"
+        self.model = None
+        self.tokenizer = None
+        self.ppl_threshold = ppl_threshold
 
+    def load_model(self):
+        if self.model is None:
+            quant_config = BitsAndBytesConfig(
+                load_in_4bit=True,
+                bnb_4bit_quant_type="nf4",
+                bnb_4bit_use_double_quant=True
+            )
+            self.model = AutoModelForCausalLM.from_pretrained(
+                self.model_id,
+                device_map="auto",
+                dtype="auto",
+                quantization_config=quant_config
+            )
+            self.model.eval()
+
+        if self.tokenizer is None:
+            self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+    
     def compute_ppl(self, text: str):
         device = next(
             self.model.parameters()
@@ -49,8 +55,9 @@ class PPLFilter(BaseFilter):
         
     def apply(self, text: str):
         text = text.strip()
+        self.load_model()
         if len(text) < 10:
             return False
 
         ppl = self.compute_ppl(text)
-        return ppl <= self.drop_ratio
+        return ppl <= self.ppl_threshold 
