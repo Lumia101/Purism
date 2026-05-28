59aef8dec56068965587911d43973b0ac3e92ccd
Siu Kim <aforever2731@gmail.com>
2026-05-29 07:42:49 +0900

    Create ultimate_pipeline.py

---
+++ purism/pipeline/ultimate_pipeline.py
@@ -0,0 +1,53 @@
+# Import all created Python codes from this repository
+import multiprocessing
+from concurrent.futures import ProcessPoolExecutor
+from typing import List, Dict, Any
+from functools import partial
+from purism.normalizers.normalizer import TextCleaner, UICleaner, UnicodeCleaner
+from purism.filters.simple_filter import LengthFilter, HarmfulWordsFilter, SpamWordsFilter, SignAbuseFilter, PIIFilter
+from purism.filters.advanced_filter import LanguageFilter, DedupFilter
+from purism.filters.model_filter import PPLFilter
+
+# Setting Settings for Data Purification
+class PurifyConfig():
+    def __init__(self, normalizer, filter_cpu, filter_gpu):
+        self.normalizer = normalizer
+        self.filter_cpu = filter_cpu
+        self.filter_gpu = filter_gpu
+
+    def normalize(self, text: str):
+        text_cleaned = text
+        for normalizer in self.normalizer:
+            text_cleaned = normalizer.normalize(text_cleaned)
+
+    def purify(self, text: list):
+        for filter in self.filters:
+            if not filter.apply(text_cleaned):
+                return {
+                    "raw_text": text,
+                    "passed": False,
+                    "filtered_by": filter.__class__.__name__,
+                    "normalized_text": text_cleaned
+                }
+
+        return {
+            "raw_text": text,
+            "passed": True,
+            "filtered_by": None,
+            "normalized_text": text_cleaned
+        }
+
+    def multi_purify(self, texts: list, n_process=-1, chunk_size=100):
+        if n_process == -1:
+            n_workers = multiprocessing.cpu_count()
+        elif n_process = 0:
+            n_workers = 1
+        elif n_process > 0:
+            n_workers = n_process
+        else:
+            raise ValueError("An invalid value was entered into n_process. ")
+        
+        with ProcessPoolExecutor(max_workers=n_workers) as executor:
+            results = list(executor.map(self.purify, texts, chunksize=chunksize))
+        
+        return results
