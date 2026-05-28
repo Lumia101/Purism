d097ac96de10e4e0610df9f61aaad678f074b0c5
Siu Kim <aforever2731@gmail.com>
2026-05-28 10:20:14 +0000

    Add multiprocessing(1)

--- purism/pipeline/pipeline.py
+++ purism/pipeline/pipeline.py
@@ -1,4 +1,8 @@
 # Import all created Python codes from this repository
+import multiprocessing
+from concurrent.futures import ProcessPoolExecutor
+from typing import List, Dict, Any
+from functools import partial
 from purism.normalizers.normalizer import TextCleaner, UICleaner, UnicodeCleaner
 from purism.filters.simple_filter import LengthFilter, HarmfulWordsFilter, SpamWordsFilter, SignAbuseFilter, PIIFilter
 from purism.filters.advanced_filter import LanguageFilter, DedupFilter
@@ -30,3 +34,12 @@ class PurifyConfig():
             "filtered_by": None,
             "normalized_text": text_cleaned
         }
+
+    def multi_purify(self, texts: str, n_process: int=None, chunk_size=100):
+        if n_process is None:
+            n_process = multiprocessing.cpu_count()
+
+        with ProcessPoolExecutor(max_workers=n_process) as executor:
+            results = list(executor.map(self.purify, texts, chunksize=chunksize))
+        
+        return results
